98dbcbca509b697760d181b962040f3992c52864
Ming Du <ming.du101@gmail.com>
2025-10-22 17:05:18 -0500

    FEAT: probe dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,10 @@
-from helpers.image_helpers import create_complex_object
+import os
+import glob
 
 import numpy as np
 from PIL import Image
+
+from helpers.image_helpers import create_complex_object
 
 
 class Dataset:
@@ -40,3 +43,36 @@ class ImageNetObjectDataset(ObjectDataset):
 class ProbeDataset(Dataset):
     pass
 
+
+class NpyProbeDataset(ProbeDataset):
+    def __init__(
+        self, root_dir: str, recursive: bool = True, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.root_dir = root_dir
+        self.index = self.create_index(recursive)
+        
+    def create_index(self, recursive: bool = True) -> list[str]:
+        """Create a list of all probe files in the root directory.
+        """
+        return glob.glob(os.path.join(self.root_dir, "*.npy"), recursive=recursive)
+        
+    def __getitem__(self, index) -> tuple[np.ndarray, str]:
+        """Get the probe and its name at the given index.
+        
+        Parameters
+        ----------
+        index : int
+            The index of the probe to get.
+
+        Returns
+        -------
+        tuple[np.ndarray, str]
+            A (n_opr_modes, n_modes, h, w) array of complex-valued probe.
+        """
+        p = np.load(self.index[index])
+        if p.ndim == 3:
+            p = p[None, ...]
+        elif p.ndim == 2:
+            p = p[None, None, ...]
+        return p
